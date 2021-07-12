8e152f7bb793a36ab48898557f259bf78e168ac3
Betina Zhou <zh.betina@gmail.com>
2021-07-12 14:54:26 +0200

    HEAL-5 add i18next config & basic translation content

---
+++ src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import eng from './translations/eng.json';
+import pl from './translations/pl.json';
+
+const resources = {
+  en: {
+    translation: eng
+  },
+  pl: {
+    translation: pl
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
