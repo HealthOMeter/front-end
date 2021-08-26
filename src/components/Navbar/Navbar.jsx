002ef2d0f7628e0c195e861275b61d0a64216b9e
Betina Zhou <zh.betina@gmail.com>
2021-08-26 14:06:13 +0200

    Config for displaying nav items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { navItems } from '../../data/navbar.data';
 import Logo from '../Logo/Logo';
 import PrimaryButton from '../PrimaryBtn/PrimaryButton';
 import { Nav, NavItems, NavLink } from './Navbar.styles';
 
 const Navbar = () => {
+    const location = useLocation();
+    const currentLoc = location.pathname;
+
     return (
         <Nav>
             <Logo />
             <NavItems>
                 {navItems.map((item, idx) => {
-                    return (
-                        <NavLink key={idx} to={item.url}>
-                           {item.isBtn ? <PrimaryButton>{item.title}</PrimaryButton> : item.title}
-                        </NavLink>
-                    );
+                    if (item.visibleOn.includes(currentLoc)){
+                        return (
+                            <NavLink key={idx} to={item.url}>
+                               {item.isBtn ? <PrimaryButton>{item.title}</PrimaryButton> : item.title}
+                            </NavLink>
+                        );
+                    }
                 })}
             </NavItems>
         </Nav>
--- src/data/navbar.data.js
+++ src/data/navbar.data.js
@@ -3,15 +3,18 @@ export const navItems = [
         title: 'About',
         url: '/app',
         isBtn: false,
+        visibleOn: ['/', '/signup', '/login']
     },
     {
         title: 'Log in',
         url: '/app',
         isBtn: false,
+        visibleOn: ['/', '/signup']
     },
     {
         title: 'Sign up',
         url: '/app',
         isBtn: true,
+        visibleOn: ['/', '/login']
     }
 ];
